b63542ab5a5f3a9b4404e080d040b1297c90b93b
Leela_Krishna <leela_krishna@dell.com>
2024-06-07 12:51:09 +0530

    app updated

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <div className="bg-white">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
