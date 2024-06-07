02fa41c3c262fb89f820e841fee0d48d90f7d560
Leela_Krishna <leela_krishna@dell.com>
2024-06-07 17:21:09 +0530

    code updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <div className="bg-white">
+      <div>
         <Navbar />
+        <Hero />
+        <Analytics />
+        <Newsletter />
+        <Cards />
+        <Footer />
       </div>
     </>
   );
